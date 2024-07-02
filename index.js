03ca24358e93446087aa8252b80a811a4cafe955
Suriya Priyan <suriyapriyan0506@gmail.com>
2024-07-02 15:30:37 +0530

    updated

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ dotenv.config();
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    
 }).then(() => {
     console.log('MongoDB connected');
 }).catch((err) => {
